ce25e8f12aaba482568ebb5331263c8dcadb57e7
Kawser Simanto <kawsersimanto@gmail.com>
2024-11-17 12:02:14 +0600

    fixed logout functionality

--- service/actions/logoutUser.ts
+++ service/actions/logoutUser.ts
@@ -1,10 +1,12 @@
-import { authKey } from '@/constants/authkey';
-import { deleteCookies } from './deleteCookies';
-import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { authKey } from "@/constants/authkey";
+import { deleteCookies } from "./deleteCookies";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { toast } from "sonner";
 
 export const logoutUser = (router: AppRouterInstance) => {
     localStorage.removeItem(authKey);
-    deleteCookies([authKey, 'token']);
-    router.push('/');
+    deleteCookies([authKey, "token"]);
+    toast.success("User logged out", { duration: 3000 });
+    router.push("/login");
     router.refresh();
 };
