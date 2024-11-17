00e9d24e6c72ddc2fd74d243123a898be4e11f72
Kawser Simanto <kawsersimanto@gmail.com>
2024-11-17 11:34:36 +0600

    set cookies and localstorage

--- components/auth/components-auth-login-form.tsx
+++ components/auth/components-auth-login-form.tsx
@@ -1,19 +1,22 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter } from 'next/navigation';
-import IconMail from '../icon/icon-mail';
-import IconLockDots from '../icon/icon-lock-dots';
-import { loginFormSchema, LoginFormValues } from '@/app/(auth)/login/LoginSchema';
-import { toast } from 'sonner';
-import { addTokenToLocalStorage } from '@/utils/tokenHandler';
-import { useLoginMutation } from '@/store/api/auth/authApi';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import IconMail from "../icon/icon-mail";
+import IconLockDots from "../icon/icon-lock-dots";
+import {
+    loginFormSchema,
+    LoginFormValues,
+} from "@/app/(auth)/login/LoginSchema";
+import { toast } from "sonner";
+import { useLoginMutation } from "@/store/api/auth/authApi";
+import setAccessToken from "@/service/actions/setAccessToken";
 
 const ComponentsAuthLoginForm = () => {
     const router = useRouter();
-    const [login, { isLoading }] = useLoginMutation();
+    const [login, { isLoading: loadingUserData }] = useLoginMutation();
 
     const {
         register,
@@ -24,7 +27,7 @@ const ComponentsAuthLoginForm = () => {
     });
 
     const onSubmit = async (data: LoginFormValues) => {
-        const loadingToastId = toast.loading('Logging in...');
+        const loadingToastId = toast.loading("Logging in...");
 
         try {
             const res = await login(data).unwrap();
@@ -34,6 +37,12 @@ const ComponentsAuthLoginForm = () => {
                     duration: 3000,
                     id: loadingToastId,
                 });
+
+                if (res?.data?.token) {
+                    localStorage.setItem("token", res?.data?.token);
+                    setAccessToken(res?.data?.token);
+                    router.push("/");
+                }
             } else {
                 toast.error(res?.message, {
                     duration: 3000,
@@ -41,7 +50,7 @@ const ComponentsAuthLoginForm = () => {
                 });
             }
         } catch (error: any) {
-            toast.error('Invalid Credentials', {
+            toast.error("Invalid Credentials", {
                 duration: 3000,
                 id: loadingToastId,
             });
@@ -49,7 +58,10 @@ const ComponentsAuthLoginForm = () => {
     };
 
     return (
-        <form className="space-y-5 dark:text-white" onSubmit={handleSubmit(onSubmit)}>
+        <form
+            className="space-y-5 dark:text-white"
+            onSubmit={handleSubmit(onSubmit)}
+        >
             <div>
                 <label htmlFor="Email">Email</label>
                 <div className="relative text-white-dark">
@@ -57,14 +69,20 @@ const ComponentsAuthLoginForm = () => {
                         id="Email"
                         type="email"
                         placeholder="Enter Email"
-                        className={`form-input ps-10 placeholder:text-white-dark ${errors.email ? 'border-red-500' : ''}`}
-                        {...register('email')}
+                        className={`form-input ps-10 placeholder:text-white-dark ${
+                            errors.email ? "border-red-500" : ""
+                        }`}
+                        {...register("email")}
                     />
                     <span className="absolute start-4 top-1/2 -translate-y-1/2">
                         <IconMail fill={true} />
                     </span>
                 </div>
-                {errors.email && <p className="mt-1 text-sm text-red-500">{errors.email.message}</p>}
+                {errors.email && (
+                    <p className="mt-1 text-sm text-red-500">
+                        {errors.email.message}
+                    </p>
+                )}
             </div>
 
             <div>
@@ -74,18 +92,28 @@ const ComponentsAuthLoginForm = () => {
                         id="Password"
                         type="password"
                         placeholder="Enter Password"
-                        className={`form-input ps-10 placeholder:text-white-dark ${errors.password ? 'border-red-500' : ''}`}
-                        {...register('password')}
+                        className={`form-input ps-10 placeholder:text-white-dark ${
+                            errors.password ? "border-red-500" : ""
+                        }`}
+                        {...register("password")}
                     />
                     <span className="absolute start-4 top-1/2 -translate-y-1/2">
                         <IconLockDots fill={true} />
                     </span>
                 </div>
-                {errors.password && <p className="mt-1 text-sm text-red-500">{errors.password.message}</p>}
+                {errors.password && (
+                    <p className="mt-1 text-sm text-red-500">
+                        {errors.password.message}
+                    </p>
+                )}
             </div>
 
-            <button type="submit" className="btn btn-gradient !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]">
-                Sign in
+            <button
+                type="submit"
+                disabled={loadingUserData}
+                className="btn btn-gradient !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]"
+            >
+                {loadingUserData ? "Signing in" : "Sign in"}
             </button>
         </form>
     );
