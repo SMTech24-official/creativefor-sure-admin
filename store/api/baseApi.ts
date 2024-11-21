f0b0e00be057dde020c7d0e30ceacaf0d51d65f8
Kawser Simanto <kawsersimanto@gmail.com>
2024-11-21 17:12:15 +0600

    changed api endpoint

--- store/api/baseApi.ts
+++ store/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
     reducerPath: "baseApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: `https://creative-for-sure-backend.vercel.app/api/v1`,
+        baseUrl: `https://creative-for-sure-backend-one.vercel.app/api/v1`,
         credentials: "include",
         prepareHeaders: (headers: any) => {
             const token = localStorage.getItem("token");
